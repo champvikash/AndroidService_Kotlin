7698a174351c2e18143a5dfaad4d53e733f932ed
Vishu <champvikash4@gmail.com>
2024-01-27 03:56:53 -0800

    Music Service

--- app/src/main/java/com/example/androidservice_kotlin/service/MusicService.kt
+++ app/src/main/java/com/example/androidservice_kotlin/service/MusicService.kt
@@ -6,29 +6,26 @@ import android.media.MediaPlayer
 import android.media.RingtoneManager
 import android.os.Build
 import android.os.IBinder
-import android.provider.Settings
 import androidx.annotation.RequiresApi
 
 
 class MusicService : Service() {
+    lateinit var mp : MediaPlayer
     override fun onBind(p0: Intent?): IBinder? {
-
         return null
-
     }
-
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         val uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
-        var mp = MediaPlayer.create(this, uri)
+        mp = MediaPlayer.create(this, uri)
         mp.isLooping = true
         mp.start()
         return START_NOT_STICKY
     }
 
     override fun onDestroy() {
-
+        mp.stop()
         super.onDestroy()
     }
 
